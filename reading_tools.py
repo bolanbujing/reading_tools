3f3fbea09430c626fb005fed340358c99c8978cd
root <1113280786@qq.com>
2017-09-22 10:16:00 +0000

    fix

--- reading_tools.py
+++ reading_tools.py
@@ -1,28 +1,34 @@
 import os
 import re
 
-pattern_select = re.compile(r'\) \{')
+pattern_select = re.compile(r'\) *\{|\) *const *\{')
+pattern_exclude = re.compile(r' +if \(| +for \(| +while \(|\) *\{.*?\}|\) *const *\{.*?\}')
 
-current_path = os.getcwd()
-file_list = os.listdir(current_path)
-for dir_ in file_list:
-	path_name = current_path+'/'+dir_
-	if os.path.isdir(path_name):
-		print path_name
-	else:
-		a,b = os.path.splitext(path_name)
-		if b=='.h' or b=='.hpp' or b=='.cpp' or b=='.cc':
-			rf = open(path_name)
-			temp_file = path_name+".temp"
-			wf = open(temp_file, 'w+')
-			wf.write("#include <iostream>\n")
-			for line in rf:
-				wf.write(line)
-				selectObj = pattern_select.search(line)
-				if selectObj:
-					wf.write("  std::cout<<__file__<<\" , \"<<__line__<<std::endl;\n")
-					print line
-			rf.close()
-			wf.close()
-			os.remove(path_name)
-			os.rename(temp_file, path_name)
+def reading (current_path):
+	file_list = os.listdir(current_path)
+	for dir_ in file_list:
+		path_name = current_path+'/'+dir_
+		if os.path.isdir(path_name):
+			reading(path_name)
+		else:
+			a,b = os.path.splitext(path_name)
+			if b=='.h' or b=='.hpp' or b=='.cpp' or b=='.cc':
+				rf = open(path_name)
+				temp_file = path_name+".temp"
+				wf = open(temp_file, 'w+')
+				wf.write("#include <iostream>\n")
+				for line in rf:
+					wf.write(line)
+					selectObj = pattern_select.search(line)
+					excludeObj = pattern_exclude.search(line)
+					if selectObj and not excludeObj:
+						wf.write("  std::cout<<__FILE__<<\" , \"<<__LINE__<<std::endl;\n")
+						print line
+				rf.close()
+				wf.close()
+				os.remove(path_name)
+				os.rename(temp_file, path_name)
+
+
+if __name__ == '__main__':
+	reading(os.getcwd())
